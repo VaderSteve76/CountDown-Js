83b8bf1b533f5c6e982f8caf1ab6d7a47809251a
Steve <alverson.s@yahoo.com>
2020-11-17 18:25:25 -0500

    Setting, removing and getting from localstorage

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const completeEl = document.getElementById('complete');
 const completeElInfo = document.getElementById('complete-info');
@@ -47,7 +48,7 @@ function updateDOM() {
       // show countdown in progress
       countdownElTitle.textContent = `${countdownTitle}`;
       timeElements[0].textContent = `${days}`;
-      imeElements[1].textContent = `${hours}`;
+      timeElements[1].textContent = `${hours}`;
       timeElements[2].textContent = `${minutes}`;
       timeElements[3].textContent = `${seconds}`;
       completeEl.hidden = true;
@@ -61,6 +62,11 @@ function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
+  savedCountdown = {
+    title: countdownTitle,
+    date: countdownDate
+  };
+  localStorage.setItem('countdown', JSON.stringify(savedCountdown));
   // Check for valid date
   if(countdownDate === '') {
     alert('Please select a date');
@@ -82,9 +88,25 @@ function reset() {
   // Reset values
   countdownTitle = '';
   countdownDate = '';
+  localStorage.removeItem('countdown');
+}
+
+// Get countdown from localstorage
+function restorePrevCountdown() {
+  if(localStorage.getItem('countdown')) {
+    inputContainer.hidden = true;
+    savedCountdown = JSON.parse(localStorage.getItem('countdown'));
+    countdownTitle = savedCountdown.title;
+    countdownDate = savedCountdown.date;
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
+  }
 }
 
 // Event listener
 countdwonForm.addEventListener('submit', updateCountdown);
 countdownBtn.addEventListener('click', reset);
 completeBtn.addEventListener('click', reset);
+
+// On load check localstorage
+restorePrevCountdown();
