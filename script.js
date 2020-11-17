3b51b3d79d987cf2545613fa21a4a037efa75350
Steve <alverson.s@yahoo.com>
2020-11-16 21:29:59 -0500

    Populating date to UI, but no countdown

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 const inputContainer = document.getElementById('input-container');
 const countdwonForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
+let countdownTitle = '';
+let countdownDate = '';
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // Set the date input min with today's date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Populate DOM / Complete UI
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+  // Populate countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+  // Hide input
+  inputContainer.hidden = true;
+  // Show countdown
+  countdownEl.hidden = false;
+}
+
+// Get values from form input
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  // Get number version of current date, update dom
+  countdownValue = new Date(countdownDate).getTime();
+  updateDOM();
+}
+
+// Event listener
+countdwonForm.addEventListener('submit', updateCountdown);
